43e0bd1cb226ceaa1adae3d7f4e22dba2e53d3b2
mohsin6862 <mohsinctg2206@gmail.com>
2023-03-07 08:37:09 +0600

    button clicked dynamically

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('I am alive')
+document.getElementById('start-btn').addEventListener('click', function(){
+
+    console.log('Clicked')
+
+});
+document.getElementById('reset-btn').addEventListener('click', function(){
+
+    console.log('Clicked')
+
+});
+document.getElementById('stop-btn').addEventListener('click', function(){
+
+    console.log('Clicked')
+
+});
